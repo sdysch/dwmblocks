e63f332bf83ded6b495743d3af224cf621653104
Samuel Dezso Dysch <sdysch94@gmail.com>
2020-06-20 23:26:23 +0100

    Add blocks

--- blocks.h
+++ blocks.h
@@ -1,20 +1,26 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
+	/*Icon*/	/*Command*/									/*Update Interval*/				/*Update Signal*/
 	
-	{"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "~/bin/statusbar/volume",						0,		10},
-
-	{"☀", "xbacklight | sed 's/\\..*//'",					0,		11},
-	
-	{"", "~/bin/statusbar/battery",						5,		0},
-
-	{"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
-
-	{"", "~/bin/statusbar/clock",						5,		0},
+	{"", "~/.local/bin/bar_scripts/music",									0,					1},
+	//{"", "xfce4-terminal -e english_to_danish --hold --drop-down",			0,					0},
+	//{"", "xfce4-terminal -e danish_to_english --hold --drop-down",			0,					0},
+	{"", "~/.local/bin/bar_scripts/clock",									30,					0},
+	{"", "~/.local/bin/bar_scripts/showcalendar",							3600,				0},
+	{"", "~/.local/bin/bar_scripts/mailbox",								180,				12},
+	{"", "~/.local/bin/bar_scripts/taskblocklet",							0,					3},
+	{"", "~/.local/bin/bar_scripts/news",									0,					6},
+	{"", "~/.local/bin/bar_scripts/volume",									0,					2},
+	{"", "~/.local/bin/bar_scripts/battery",								10,					0},
+	{"", "~/.local/bin/bar_scripts/battery",								10,					0},
+	{"", "~/.local/bin/bar_scripts/internet",								30,					0},
+	{"", "~/.local/bin/bar_scripts/weatherreport",							18000,				5},
+	{"", "~/.local/bin/bar_scripts/keyindicator",							0,					11},
+	{"💻", "~/.local/bin/bar_scripts/cpu_usage",							15,					0},
+	{"📁", "~/.local/bin/bar_scripts/disk_usage",							600,				0},
+	{"⏱️", "uptime -p | sed 's/hours/h/; s/minutes/min/; s/seconds/sec/'",	60,					0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim = '|';
+static char *delim = " ";
