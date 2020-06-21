017903b1e1c0c7da521d4cb55c26b7b6b1ffdb7e
Samuel Dezso Dysch <sdysch94@gmail.com>
2020-06-21 12:24:32 +0100

    Simplify

--- blocks.h
+++ blocks.h
@@ -6,15 +6,15 @@ static const Block blocks[] = {
 	// https://fontawesome.com/cheatsheet?from=io
 
 	{"", "~/.local/bin/dwmblocks_scripts/music",									0,					1},
-	{"", "~/.local/bin/dwmblocks_scripts/clock",									30,					0},
-	{"", "~/.local/bin/dwmblocks_scripts/showcalendar",							3600,				0},
+	{"", "~/.local/bin/dwmblocks_scripts/clock",									30,					0},
+	{"", "~/.local/bin/dwmblocks_scripts/showcalendar",							3600,				0},
 	{"", "~/.local/bin/dwmblocks_scripts/taskblocklet",							0,					3},
-	{"", "~/.local/bin/dwmblocks_scripts/news",									0,					6},
+	{"", "~/.local/bin/dwmblocks_scripts/news",									0,					6},
 	{"", "~/.local/bin/dwmblocks_scripts/volume",									0,					2},
 	{"", "~/.local/bin/dwmblocks_scripts/betterbattery",							10,					0},
 	{"", "~/.local/bin/dwmblocks_scripts/internet",									30,					0},
-	//{"", "~/.local/bin/dwmblocks_scripts/cpu_usage",								15,					0},
-	//{"", "~/.local/bin/dwmblocks_scripts/disk_usage",							600,				0},
+	{"", "~/.local/bin/dwmblocks_scripts/cpu_usage",								15,					0},
+	{"", "~/.local/bin/dwmblocks_scripts/disk_usage",							600,				0},
 	{"", "uptime -p | sed 's/hours/h/; s/minutes/min/; s/seconds/sec/'",			60,					0},
 };
 
