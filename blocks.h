cab7fa6334bbbf8416f23be2885d04942a343b0f
Samuel Dezso Dysch <sdysch94@gmail.com>
2020-12-20 22:51:29 +0000

    Update blocks. Add package updates

--- blocks.h
+++ blocks.h
@@ -6,8 +6,6 @@ static const Block blocks[] = {
 	/*Icon*/	    /*Command*/									                   /*Update Interval*/	   /*Update Signal*/
 
 	//{"",            "~/.local/bin/dwmblocks_scripts/music",									0,					1},
-	{"",            "~/.local/bin/dwmblocks_scripts/clock",									30,					0},
-	{"",            "~/.local/bin/dwmblocks_scripts/showcalendar",							3600,				0},
 	//{"",            "~/.local/bin/dwmblocks_scripts/taskblocklet",							0,					3},
 	//{"",            "~/.local/bin/dwmblocks_scripts/news",									0,					6},
 	{"",            "~/.local/bin/dwmblocks_scripts/volume",								0,					2},
@@ -15,9 +13,12 @@ static const Block blocks[] = {
 	{"",            "~/.local/bin/dwmblocks_scripts/betterbattery",							10,					0},
 	{"",			"~/.local/bin/dwmblocks_scripts/internet",								30,					0},
 	{"",			"~/.local/bin/dwmblocks_scripts/weatherreport",							18000,				5},
-	{" ",          "~/.local/bin/dwmblocks_scripts/cpu_usage",								15,					0},
-	{"﫭 ",         "~/.local/bin/dwmblocks_scripts/disk_usage",							600,				0},
-	{"",            "uptime -p | sed 's/hours/h/; s/minutes/min/; s/seconds/sec/'",			60,					0},
+	{"",            "~/.local/bin/dwmblocks_scripts/package_updates",						18000,				0},
+	{"",            "~/.local/bin/dwmblocks_scripts/cpu_usage",								15,					0},
+	{"",            "~/.local/bin/dwmblocks_scripts/disk_usage",							600,				0},
+	{"",            "~/.local/bin/dwmblocks_scripts/uptime",			                    60,					0},
+	{"",            "~/.local/bin/dwmblocks_scripts/clock",									30,					0},
+	{"",            "~/.local/bin/dwmblocks_scripts/showcalendar",							3600,				0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
