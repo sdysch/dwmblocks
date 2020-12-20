3948bf228d650fff41d7d574ec7cfc7d420df956
Samuel Dezso Dysch <sdysch94@gmail.com>
2020-12-20 23:13:03 +0000

    Reorder blocks

--- blocks.h
+++ blocks.h
@@ -8,17 +8,17 @@ static const Block blocks[] = {
 	//{"",            "~/.local/bin/dwmblocks_scripts/music",									0,					1},
 	//{"",            "~/.local/bin/dwmblocks_scripts/taskblocklet",							0,					3},
 	//{"",            "~/.local/bin/dwmblocks_scripts/news",									0,					6},
-	{"",            "~/.local/bin/dwmblocks_scripts/volume",								0,					2},
 	{"",            "~/.local/bin/dwmblocks_scripts/mailbox",								180,				12},
-	{"",            "~/.local/bin/dwmblocks_scripts/betterbattery",							10,					0},
-	{"",			"~/.local/bin/dwmblocks_scripts/internet",								30,					0},
-	{"",			"~/.local/bin/dwmblocks_scripts/weatherreport",							18000,				5},
-	{"",            "~/.local/bin/dwmblocks_scripts/package_updates",						18000,				0},
 	{"",            "~/.local/bin/dwmblocks_scripts/cpu_usage",								15,					0},
 	{"",            "~/.local/bin/dwmblocks_scripts/disk_usage",							600,				0},
+	{"",            "~/.local/bin/dwmblocks_scripts/package_updates",						18000,				0},
 	{"",            "~/.local/bin/dwmblocks_scripts/uptime",			                    60,					0},
-	{"",            "~/.local/bin/dwmblocks_scripts/clock",									30,					0},
+	{"",			"~/.local/bin/dwmblocks_scripts/weatherreport",							18000,				5},
+	{"",			"~/.local/bin/dwmblocks_scripts/internet",								30,					0},
+	{"",            "~/.local/bin/dwmblocks_scripts/volume",								0,					2},
+	{"",            "~/.local/bin/dwmblocks_scripts/betterbattery",							10,					0},
 	{"",            "~/.local/bin/dwmblocks_scripts/showcalendar",							3600,				0},
+	{"",            "~/.local/bin/dwmblocks_scripts/clock",									30,					0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
